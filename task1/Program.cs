f335b04627e4241815c8291d9932d4f7ccc58202
Soulbring <lobakin_vova@vk.com>
2024-08-26 14:23:49 +0400

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Создайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+
+
+void Main()
+{
+    char[,] letter = new char[,]
+    {
+    { 'a', 'b', 'c', 'd', 'r'},
+    {'e','u','i','o','a'}
+    };
+
+System.Console.Write(FromCharArrayToString(letter));
+
+
+}
+
+
+string FromCharArrayToString(char[,] array)
+
+{
+    string str = "";
+    foreach (char c in array)
+        str += c;
+        return str;
+}
+
+Main();
