eead7e1a2810a7f74b440ef5edb52a79da4bcd60
Soulbring <lobakin_vova@vk.com>
2024-08-26 14:37:02 +0400

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Создайте строкуб сщдержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+void Main()
+{
+    Console.WriteLine("ВВедите текст: ");
+    string? text = System.Console.ReadLine()!.ToLower();
+
+    System.Console.Write(text);
+
+}
+
+
+Main();
