388b3ff7c7929cb8fa5ddcb02f2de5f328d12ca9
Soulbring <lobakin_vova@vk.com>
2024-08-27 08:42:53 +0400

    add task 4

---
+++ task4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В полученной строке слова
+// должны быть также разделены пробелами.
+
+void Main()
+{
+
+Console.WriteLine("ВВедите текст через пробелы: ");
+string? letter = Console.ReadLine();
+string[] LetterArray = letter.Split();
+Array.Reverse(LetterArray);
+letter = string.Join(" ", LetterArray);
+Console.Write(letter);
+
+}
+
+
+
+Main();
