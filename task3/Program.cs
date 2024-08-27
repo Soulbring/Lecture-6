7636b7705a16253fe64317a4ddacdac0a0306ceb
Soulbring <lobakin_vova@vk.com>
2024-08-27 08:46:26 +0400

    add task 3

---
+++ task3/Program.cs
@@ -0,0 +1,43 @@
+﻿//Создайте произвольную строку. Выясните, являеться ли она полиндромом
+
+void Main()
+{
+    Console.WriteLine("ВВедите текст: ");
+    string? text = System.Console.ReadLine()!.ToLower();
+    CheckPoly(text);
+}
+
+
+void CheckPoly(string text)
+
+{
+
+    char[] ValidateText = text.ToCharArray();
+    int i = 0;
+    while (true)
+    {
+
+        if (ValidateText[i] == ValidateText[ValidateText.Length - 1 - i])
+        {
+            if (i <= ValidateText.Length - i)
+            {
+                i++;
+            }
+            else
+            {
+                System.Console.WriteLine("Являеться полиндромом");
+                break;
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("НЕ являеться полиндромом");
+            break;
+        }
+
+
+    }
+}
+
+
+Main();
